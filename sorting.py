3eddcd81e30bd40fc1d8f0cc71bf1df7badb1fca
Ondrej Vesely <265290@vutbr.cz>
2026-04-23 15:22:37 +0200

    plplp

--- sorting.py
+++ sorting.py
@@ -6,16 +6,34 @@ def random_numbers(count, low=0, high=100):
 def selection_sort(numbers):
     nums = numbers.copy()
     delka = len(nums)
-    serazeny = []
+    # serazeny = []
     for i in range(delka):
         m = i
         for j in range(i,delka):
             if nums[j] < nums[m]:
                 m = j
-                serazeny.append(j)
+                # serazeny.append(j)
         nums[i], nums[m] = nums[m], nums[i]
     return nums
 
+# def selection_sort(numbers):
+#     nums = numbers.copy()
+#     delka = len(nums)
+#     i = 1
+#     for j in range(i,delka):
+#         if nums[j] < nums[i]:
+#             i= j
+#     nums[j], nums[i] = nums[i], nums[j]
+#     return nums
+
+def bubble_sort(numbers):
+    nums = numbers.copy()
+    n = len(nums)
+    for i in range(n-1):
+        for j in range(n-1):
+            if nums[j]>nums[j+1]:
+                nums[j], nums[j+1] = nums[j+1], nums[j]
+    return nums
 
 if __name__ == "__main__":
     values = random_numbers(10)  # 10 čísel v rozsahu 0–100
@@ -28,3 +46,7 @@ if __name__ == "__main__":
     print(sorting)
     numb = sorted(sorting)
     print(numb)
+    bubble = bubble_sort([5, 1, 4, 2, 8])
+    print(bubble)
+    bubbles = bubble_sort(random_numbers(10))
+    print(bubbles)
