c37b35901dd1074b76cc67b64a4994f2f98eff96
Ondrej Vesely <265290@vutbr.cz>
2026-04-23 14:57:42 +0200

    plplp

--- sorting.py
+++ sorting.py
@@ -3,9 +3,28 @@ import random
 def random_numbers(count, low=0, high=100):
     return [random.randint(low, high) for _ in range(count)]
 
+def selection_sort(numbers):
+    nums = numbers.copy()
+    delka = len(nums)
+    serazeny = []
+    for i in range(delka):
+        m = i
+        for j in range(i,delka):
+            if nums[j] < nums[m]:
+                m = j
+                serazeny.append(j)
+        nums[i], nums[m] = nums[m], nums[i]
+    return nums
+
+
 if __name__ == "__main__":
     values = random_numbers(10)  # 10 čísel v rozsahu 0–100
     print(values)  # např. [42, 7, 91, 15, 63, 8, 57, 73, 2, 100]
-
     small = random_numbers(5, low=0, high=20)  # 5 čísel v rozsahu 0–20
     print(small)
+    sort = selection_sort([5, 1, 4, 2, 8, 10, 20,18, 15])
+    print(sort)
+    sorting = selection_sort(random_numbers(10))
+    print(sorting)
+    numb = sorted(sorting)
+    print(numb)
