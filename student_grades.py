e59766610f9232ad1fa711c296f03b89a7fd0cc8
Ondrej Vesely <265290@vutbr.cz>
2026-04-23 16:36:46 +0200

    konec

--- student_grades.py
+++ student_grades.py
@@ -1,3 +1,4 @@
+from email.base64mime import body_decode
 
 
 class StudentsGrades:
@@ -10,7 +11,48 @@ class StudentsGrades:
     def count(self):
         return len(self.scores)
 
-    for i in results:
+    def get_grade(self, index):
+        body = self.scores[index]
+        if 90 <= body <= 100:
+            return "A"
+        elif 80 <= body <= 89:
+            return "B"
+        elif 70 <= body <= 79:
+            return "C"
+        elif 60 <= body <= 69:
+            return "D"
+        elif 50 <= body <= 59:
+            return "E"
+        elif 0 <= body <= 49:
+            return "F"
+
+    def find(self, body):
+        for i in range(len(self.scores)):
+            if self.scores[i] == body:
+                kolikaty = i
+        return kolikaty
+
+    def get_sorted(self):
+        nums = self.scores.copy()
+        n = len(nums)
+        for i in range(n - 1):
+            for j in range(n - 1):
+                if nums[j] > nums[j + 1]:
+                    nums[j], nums[j + 1] = nums[j + 1], nums[j]
+        return nums
+
+def main():
+    results = StudentsGrades([85, 42, 91, 67, 50, 73, 100, 38, 58])
+    pocet = results.count()
+    vypsat_indexy= []
+    for index, i in enumerate(results.scores):
+        znamka = results.get_grade(index)
+        print(f"Student {index}: {i} points - {znamka}")
+        if i == 100:
+            vypsat_indexy.append(index)
+    print(vypsat_indexy)
+    serazeny = results.get_sorted()
+    print(serazeny)
 
 if __name__ == "__main__":
     results = StudentsGrades([85, 42, 91, 67, 50, 73, 100, 38, 58])
@@ -18,4 +60,7 @@ if __name__ == "__main__":
     print(results.count())          # 9
     print(results.get_by_index(2))  # 91
     print(results.scores)           # [85, 42, 91, 67, 50, 73, 100, 38, 58]
-
+    print(results.get_grade(2))
+    print(results.find(42))
+    print(results.get_sorted())
+    print(main())
